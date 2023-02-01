efa3efb339ba84dc968ab153fe7cff7e29ec680b
Moyinoluwa Shabi <shabimoyinoluwa19@gmail.com>
2023-02-01 11:35:27 +0100

    First Commit- Implemented SplashPage UI

---
+++ android/app/src/main/kotlin/com/example/mounts_of_the_world_app_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mounts_of_the_world_app_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+const Color mainColor = Color(0xFFFF5656);
+
+void main() {
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashPage(),
+  ));
+}
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+      const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+    );
+    return Container(
+      color: mainColor,
+      child: Stack(
+        children: [
+          const Align(
+            alignment: Alignment.center,
+            child: Icon(
+              Icons.terrain_rounded,
+              color: Colors.white,
+              size: 90,
+            ),
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              margin: const EdgeInsets.only(bottom: 80),
+              child: const CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
